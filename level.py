9e9f6845da7f7e978c636be0920592b11a7548f2
Daniil Orchikov <95042131+DaniilOrchikov@users.noreply.github.com>
2026-02-12 21:23:09 +0300

    Fix draw layering and refine lighting/shadow behavior

--- level.py
+++ level.py
@@ -28,32 +28,33 @@ class Level:
         self.level[pos[1]][pos[0]] = GameScreen(*pos, tree_count=self.tree_count)
         self.set_of_screen_pos.add(pos)
 
-    def paint_shadows(self):
+    def visible_screens(self):
         player_pos = self.manager.player_rect.x // WIDTH, self.manager.player_rect.y // HEIGHT
+        arr = []
         for i in range(-1, 2):
             for j in range(-1, 2):
                 pos = (player_pos[0] + i, player_pos[1] + j)
                 if pos not in self.set_of_screen_pos:
                     self.create_screen(pos)
-                self.level[pos[1]][pos[0]].paint_shadows(self.screen, self.manager.scroll)
+                arr.append(self.level[pos[1]][pos[0]])
+        return arr
 
     def paint(self):
-        player_pos = self.manager.player_rect.x // WIDTH, self.manager.player_rect.y // HEIGHT
-        for i in range(-1, 2):
-            for j in range(-1, 2):
-                pos = (player_pos[0] + i, player_pos[1] + j)
-                if pos not in self.set_of_screen_pos:
-                    self.create_screen(pos)
-                self.level[pos[1]][pos[0]].paint(self.screen, self.manager.scroll, self.manager, (i, j) == (0, 0))
+        entities = []
+        for game_screen in self.visible_screens():
+            for tree in game_screen.tree_arr:
+                y_sort = tree.y + (tree.im.get_height() if tree.hp > 0 else tree.stump_im.get_height())
+                entities.append((y_sort, lambda t=tree: t.paint(self.screen, self.manager.scroll, self.manager)))
+
+        entities.append((self.manager.player_rect.y + self.manager.player_rect.height, self.manager.paint_player))
+        entities.sort(key=lambda item: item[0])
+        for _, draw in entities:
+            draw()
 
     @property
     def physics_arr(self):
         arr = []
-        for i in range(-1, 2):
-            for j in range(-1, 2):
-                game_screen = self.level[self.manager.player_rect.y // HEIGHT + i][
-                    self.manager.player_rect.x // WIDTH + j]
-                if game_screen is not None:
-                    for tree in game_screen.tree_arr:
-                        arr.append(tree)
+        for game_screen in self.visible_screens():
+            for tree in game_screen.tree_arr:
+                arr.append(tree)
         return arr
